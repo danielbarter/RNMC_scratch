0967fa6e3cca5a378066c56220371afa9f6e26d5
Daniel Barter <danielbarter@gmail.com>
2021-02-12 11:39:46 -0800

    ..

---
+++ test_goal.py
@@ -0,0 +1,21 @@
+import sys
+sys.path.append('./mrnet/src')
+from mrnet.network.reaction_generation import *
+from monty.serialization import loadfn
+import pickle
+
+molecule_list_json = sys.argv[1]
+
+molecule_entries = loadfn(molecule_list_json)
+reaction_generator = ReactionGenerator(molecule_entries)
+
+produced_reactions = []
+
+for reaction in reaction_generator:
+    produced_reactions.append((frozenset(reaction.reactant_ids),
+                               frozenset(reaction.product_ids)))
+
+produced_reactions = frozenset(produced_reactions)
+
+with open('./goal','rb') as f:
+    goal = pickle.load(f)
