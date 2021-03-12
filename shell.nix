bb497d9b770b2d8940b0e12fbcbeea173daaccb7
Daniel Barter <danielbarter@gmail.com>
2021-03-12 12:16:11 -0800

    ..

--- shell.nix
+++ shell.nix
@@ -4,6 +4,9 @@ with (import /home/danielbarter/nixpkgs {});
 let
   python = let
     packageOverrides = self: super: {
+
+
+
       pymatgen = super.pymatgen.overridePythonAttrs (
         old: { version = "2022.0.4";
                src = super.fetchPypi {
@@ -11,10 +14,27 @@ let
                  version = "2022.0.4";
                  extension = "tar.gz";
                  sha256 = "0x05glcgczyjrmfrigsrm6nb7d9whfk6ilralqwn17hp2264jhbj";};
-               checkInputs = [ super.hypothesis super.pytest ];
+               checkInputs = [ super.pytest ];
              }
       );
 
+      numpy = super.numpy.overridePythonAttrs (
+        old: { version = "1.20.1";
+               src = super.fetchPypi {
+                 pname = "numpy";
+                 version = "1.20.1";
+                 extension = "zip";
+                 sha256 = "02m6sms6wb4flfg8y4h0msan4y7w7qgfqxhdk21lcabhm2339iiv";};
+               checkInputs = [ super.hypothesis super.pytest ];
+               patches = [];
+             }
+      );
+
+      pandas = super.pandas.overridePythonAttrs (
+        old: {
+          disabledTests = old.disabledTests ++ ["test_loc_setitem_empty_append_raises"];
+        }
+      );
 
 
     }; in python38.override {inherit packageOverrides;};
