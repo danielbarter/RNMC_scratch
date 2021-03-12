1af2714a3233b0fcdd441da46cdb7c1616688eab
Daniel Barter <danielbarter@gmail.com>
2021-03-12 08:44:00 -0800

    ..

--- shell.nix
+++ shell.nix
@@ -1,6 +1,25 @@
 with (import /home/danielbarter/nixpkgs {});
 
-let pythonEnv = python38.withPackages (
+
+let
+  python = let
+    packageOverrides = self: super: {
+      pymatgen = super.pymatgen.overridePythonAttrs (
+        old: { version = "2022.0.4";
+               src = super.fetchPypi {
+                 pname = "pymatgen";
+                 version = "2022.0.4";
+                 extension = "tar.gz";
+                 sha256 = "0x05glcgczyjrmfrigsrm6nb7d9whfk6ilralqwn17hp2264jhbj";};
+               checkInputs = [ super.hypothesis super.pytest ];
+             }
+      );
+
+
+
+    }; in python38.override {inherit packageOverrides;};
+
+  pythonEnv = python.withPackages (
       ps: [ ps.pymatgen
             ps.numpy
             ps.openbabel-bindings
