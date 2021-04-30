6e9ebae5ab1d8b814414e6e6f36a2a13681073ce
Daniel Barter <danielbarter@gmail.com>
2021-04-30 09:45:05 -0700

    ..

--- serialize.py
+++ serialize.py
@@ -17,42 +17,42 @@ network_folder = sys.argv[2]
 params_folder = sys.argv[3]
 
 
-# li_plus_mol_entry = find_mol_entry_from_xyz_and_charge(
-#     molecule_entries,
-#     './xyz_files/Li.xyz',
-#     1)
+li_plus_mol_entry = find_mol_entry_from_xyz_and_charge(
+    molecule_entries,
+    './xyz_files/Li.xyz',
+    1)
 
-# ec_mol_entry = find_mol_entry_from_xyz_and_charge(
-#     molecule_entries,
-#     './xyz_files/EC.xyz',
-#     0)
+ec_mol_entry = find_mol_entry_from_xyz_and_charge(
+    molecule_entries,
+    './xyz_files/EC.xyz',
+    0)
 
-# ledc_mol_entry  = find_mol_entry_from_xyz_and_charge(
-#     molecule_entries,
-#     './xyz_files/LEDC.xyz',
-#     0)
+ledc_mol_entry  = find_mol_entry_from_xyz_and_charge(
+    molecule_entries,
+    './xyz_files/LEDC.xyz',
+    0)
 
-# initial_state_data_0 = [
-# (li_plus_mol_entry, 30),
-# (ec_mol_entry,30)
-# ]
+initial_state_data_0 = [
+(li_plus_mol_entry, 30),
+(ec_mol_entry,30)
+]
 
-# initial_state_data_1 = [
-# (li_plus_mol_entry, 300),
-# (ec_mol_entry,30)
-# ]
+initial_state_data_1 = [
+(li_plus_mol_entry, 300),
+(ec_mol_entry,30)
+]
 
-# initial_state_data_2 = [
-# (li_plus_mol_entry, 30),
-# (ec_mol_entry,300)
-# ]
+initial_state_data_2 = [
+(li_plus_mol_entry, 30),
+(ec_mol_entry,300)
+]
 
-# initial_state_data_3 = [
-# (li_plus_mol_entry, 300),
-# (ec_mol_entry,300)
-# ]
+initial_state_data_3 = [
+(li_plus_mol_entry, 300),
+(ec_mol_entry,300)
+]
 
 reaction_generator = ReactionGenerator(molecule_entries, single_elem_interm_ignore=[])
 SerializeNetwork(network_folder,reaction_generator,constant_barrier=0.1, shard_size=2000000)
-# serialize_initial_state(network_folder, molecule_entries, initial_state_data_0)
-# serialize_simulation_parameters(params_folder, number_of_threads=7, step_cutoff=200, number_of_simulations = 10000)
+serialize_initial_state(network_folder, molecule_entries, initial_state_data_0)
+serialize_simulation_parameters(params_folder, number_of_threads=7, step_cutoff=200, number_of_simulations = 10000)
